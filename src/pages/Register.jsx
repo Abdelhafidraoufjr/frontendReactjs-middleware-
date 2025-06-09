9ca3d6c4940b2ee88644e4fbebedc7a1befb9c37
adoulServer <adouladoul14@gmail.com>
2025-06-09 20:39:19 +0100

    update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,7 +14,6 @@ const Register = () => {
     });
 
     const [darkMode, setDarkMode] = useState(false);
-
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
